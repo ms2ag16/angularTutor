c259835a5ab0994110a901a5325c25db16930e40
Mi Sun <msun85@gatech.edu>
2019-01-20 15:07:08 -0500

    create homework1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
+import { Homework1Component } from './homework1/homework1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    Homework1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/homework1/homework1.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Homework1Component } from './homework1.component';
+
+describe('Homework1Component', () => {
+  let component: Homework1Component;
+  let fixture: ComponentFixture<Homework1Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Homework1Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Homework1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
